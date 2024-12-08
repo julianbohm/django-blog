11efc3578d1588ae299929a9eadce46258038b82
Julian Bohm <julianbohm9@gmail.com>
2024-12-08 11:16:35 +0000

    add a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-julianbohm-djangoblog-vi9wwe9td35.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8080-julianbohm-djangoblog-vi9wwe9td35.ws.codeinstitute-ide.net','8000-julianbohm-djangoblog-vi9wwe9td35.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
@@ -88,6 +88,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 
 # Password validation
