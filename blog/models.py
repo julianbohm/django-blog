25086ab27f1cd061dfc18f0474d568f2e97ccc45
Julian Bohm <julianbohm9@gmail.com>
2024-12-09 12:18:50 +0000

    fix comment to Comment

--- blog/models.py
+++ blog/models.py
@@ -16,11 +16,11 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-class comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
-        User, ondelete=models.CASCADE, related_name="commenter")
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
