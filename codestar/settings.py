0926ea3f6cb32a8adc4d76f6b180e4aa64544cb0
Julian Bohm <julianbohm9@gmail.com>
2024-12-12 10:15:16 +0000

    basic view

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8080-julianbohm-djangoblog-vi9wwe9td35.ws.codeinstitute-ide.net','8000-julianbohm-djangoblog-vi9wwe9td35.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
